0579faac70492e29955f0f1a634ae950d1efe821
Henry Heberle <heberleh.public@gmail.com>
2019-05-11 16:39:43 -0300

    Small setups

--- solution.py
+++ solution.py
@@ -136,26 +136,27 @@ for train_index, test_index in rs.split(X):
 
     # Various hyper-parameters to tune
     xgb1 = XGBRegressor()
-    parameters = {'nthread':[1], #when use hyperthread, xgboost may become slower
+    parameters = {'nthread':[2], #when use hyperthread, xgboost may become slower
                 'objective':['reg:linear'],
                 'learning_rate': [.03, 0.05, .07], #so called `eta` value
                 'max_depth': [5, 6, 7],
                 'min_child_weight': [4],
                 'silent': [1],
+                'booster': ['gbtree', 'gblinear', 'dart'],
                 'subsample': [0.7],
                 'colsample_bytree': [0.7],
-                'n_estimators': [128, 512]}
+                'n_estimators': [512, 1024, 1536]}
 
     xgb_grid_sel_f_i = GridSearchCV(xgb1,
                             parameters,
                             cv = 5,
-                            n_jobs = 5,
+                            n_jobs = 4,
                             verbose=False)
 
     xgb_grid_sel_f_u = GridSearchCV(xgb1,
                             parameters,
                             cv = 5,
-                            n_jobs = 5,
+                            n_jobs = 4,
                             verbose=False)
 
     xgb_grid = GridSearchCV(xgb1,
@@ -173,9 +174,12 @@ for train_index, test_index in rs.split(X):
 
     print("Selected features score u: ", xgb_grid_sel_f_u.best_score_)
     print(xgb_grid.best_params_)
+    print("Number of features: ", len(X_train[selected_features_union].columns))
 
     print("Selected features score i: ", xgb_grid_sel_f_i.best_score_)
     print(xgb_grid.best_params_)
+    print("Number of features: ", len(X_train[selected_features_intersection].columns))
 
     print("All features score: ", xgb_grid.best_score_)
     print(xgb_grid.best_params_)
+    print("Number of features: ", len(X_train.columns))
